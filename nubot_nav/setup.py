524a4c3d65c5c4aa3cb5637b3f068b088575d2e1
sayantani <sayantanibhattacharya2025@u.northwestern.edu>
2024-11-28 16:27:02 -0600

    slam toolbox working.

--- nubot_nav/setup.py
+++ nubot_nav/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),                                   
-        # ('share/' + package_name + '/config', ['config/nubot_urdf.rviz']),
+        ('share/' + package_name + '/config', ['config/nubot_urdf.rviz', 'config/slam_params_async.yaml']),
         # ('share/' + package_name + '/urdf', ['urdf/nubot.urdf.xacro', 'urdf/nubot.gazebo.xacro']),
         # ('share/' + package_name + '/env-hooks', ['env-hooks/nubot.dsv']),
         # ('share/' + package_name + '/worlds', ['worlds/nubot_world.sdf', 'worlds/nubot_simple.sdf']),
